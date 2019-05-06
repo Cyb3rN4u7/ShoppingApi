1725989b8d05a4bb7a4686a630d5adf4c6f7f9ba
Achilles Moraites <achimoraites@yahoo.gr>
2019-05-06 19:27:06 +0300

    route: GET api/Products

---
+++ ShoppingApi/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using ShoppingApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        Product[] products = new Product[]
+        {
+            new Product {Id = 1, Name = "Green Matter Keyboard", Category = "Electronics", Price = 60},
+            new Product {Id = 2, Name = "Red Monster Cereal", Category = "Food", Price = 9.99M},
+            new Product {Id = 3, Name = "Plastic Box", Category = "House", Price = 12.99M},
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            return products;
+        }
+    }
+}
